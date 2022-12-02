97a54c583cd5663b2a803deff04317ea368d9056
FatimaALzahrani <107775566+FatimaALzahrani@users.noreply.github.com>
2022-12-02 15:24:33 +0300

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -6,25 +6,22 @@ class Solution{
 		
 		while (sc.hasNext()) {
 			String input=sc.next();
-            System.out.println(isBalanced(input));
+            System.out.println(isbalanced(input));
 		}
 	}
-      static boolean isBalanced(String parentheses) {
- Stack <Character> stack = new Stack<Character>();
-            for (int i = 0; i < parentheses.length(); i++) {
-                char ch = parentheses.charAt(i);
-                if (ch == '(' || ch == '[' || ch == '{') {
-                    stack.push(ch);
-                } else if (stack.empty()) {
-                    return false;
-                } else {
-                    char top = stack.pop();
-                    if ((top == '(' && ch != ')') || (top == '[' && ch != ']')
-                            || (top == '{' && ch != '}')) {
-                        return false;
-                    }
-                }
+    public static boolean isbalanced(String input){
+        Stack<Character> st=new Stack <Character>();
+        for(int i=0 ; i<input.length();i++){
+            if(input.charAt(i)=='(' || input.charAt(i)=='[' || input.charAt(i)=='{'){
+            st.push(input.charAt(i));
+            }else if(st.empty()){
+                return false;
+            }else{
+                char top=st.pop();
+                if(top=='(' && input.charAt(i)!=')' || top=='[' && input.charAt(i)!=']' || top=='{' && input.charAt(i)!='}' )
+                return false;
             }
-            return stack.empty();
         }
+                    return st.empty();        
+    }
     }
